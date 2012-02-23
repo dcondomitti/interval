4ed1cee37654fb36d1b696499924a07d77ae14f1
Dan Condomitti <dan@interval.io>
2012-02-23 00:57:46 -0800

    Complete rename.

--- lib/concurrent.rb
+++
@@ -1,9 +0,0 @@
-require 'concurrent/version'
-require 'concurrent/middleware/timing'
-require 'concurrent/config'
-require 'concurrent/railtie' if defined? ::Rails::Railtie
-require 'concurrent/engine' if defined? ::Rails::Engine
-
-module Concurrent
-
-end
--- lib/concurrent/version.rb
+++
@@ -1,3 +0,0 @@
-module Concurrent
-  VERSION = '0.0.1'
-end
