e87ebe4acc42123bba30dd8e199addc42117453a
Dan Condomitti <dan@interval.io>
2012-02-22 21:33:16 -0800

    Move timing middleware before Rack::Runtime.

--- lib/concurrent/railtie.rb
+++ lib/concurrent/railtie.rb
@@ -3,7 +3,7 @@ require 'rails'
 module Concurrent
   class Railtie < Rails::Railtie
     initializer 'concurrent.inject_middleware' do |app|
-      app.config.middleware.insert_before 'Rails::Rack::Logger', 'Concurrent::Middleware::Timing'
+      app.config.middleware.insert_before Rack::Runtime, Concurrent::Middleware::Timing
     end
   end
 end
