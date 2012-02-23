1b2ce0c43d609d845436df55d679f660617c0504
Dan Condomitti <dan@interval.io>
2012-02-22 22:18:18 -0800

    Change initializer name to better reflect what it actually does.

--- lib/concurrent/engine.rb
+++ lib/concurrent/engine.rb
@@ -1,6 +1,6 @@
 module Concurrent
   class Engine < Rails::Engine
-    initializer 'concurrent.inject_before_filter', :after => :load_config_initializers do |app|
+    initializer 'concurrent.inject_controller_helpers', :after => :load_config_initializers do |app|
 
       # Inject our helpers into all controllers that inherit from ActionController
       # (basically every one.) self#included methods in the helpers module automatically
@@ -9,7 +9,7 @@ module Concurrent
         require 'concurrent/helpers'
         include Concurrent::Helpers
       end
-      
+
     end
   end
 end
